00c5c078138b38ddaadcfb8b3ff33402796e403c
Roshni <roshnisahu11222@gmail.com>
2026-07-19 15:49:33 +0530

    Create First HelloWorld Program

---
+++ 1_HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+// 1st
+
+class HelloWorld{
+     public static void main(String[] args) {
+        // Output
+        System.out.print("Hello World!");  // Use Ln for next line
+        System.out.println("Hello World!");
+        System.out.print("Hello World with java \n from \n Apna College.");
+    }
+}
