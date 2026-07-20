71d5da396b3a6af8c2ae8614e68c7510abe43e4c
Roshni <roshnisahu11222@gmail.com>
2026-07-20 22:54:51 +0530

    Added Variables Concepts

---
+++ 2_Variables/Variable.java
@@ -0,0 +1,19 @@
+// 3rd
+
+public class Variable {
+    public static void main(String[] args) {
+        // variables & primitive Data types
+        String name = "Rani";
+        double price = 20.25;
+
+        name = "Roshni";
+
+        System.out.println("Name:" + name);
+        System.out.println("Price :" + price);
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Result :" + sum);
+    }
+}
