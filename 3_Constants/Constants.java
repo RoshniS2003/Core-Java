4b36f8f4dbd5d37256969f61b5241e3478ae94b7
Roshni <roshnisahu11222@gmail.com>
2026-07-22 15:44:00 +0530

    Added Constants Concepts

---
+++ 3_Constants/Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+    public static void main(String[] args) {
+        // Constants:--->> Values cannot be change
+        // In Java we use final not const keyword 
+        final float PI = 3.14F;
+        System.out.println(PI);
+    }
+}
