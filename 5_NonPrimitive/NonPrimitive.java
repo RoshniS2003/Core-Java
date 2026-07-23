3d2a815c75a003f4413ef5ea5c0b6c39be7a7efa
Roshni <roshnisahu11222@gmail.com>
2026-07-23 10:51:50 +0530

    Added NonPrimitive Datatypes Concepts

---
+++ 5_NonPrimitive/NonPrimitive.java
@@ -0,0 +1,16 @@
+// 6th
+
+public class NonPrimitive {
+    public static void main(String[] args) {
+        String name = new String("Hello");
+        System.out.println("Name:" + name);
+
+        int[] marks = new int[3];
+        marks[0] = 80;
+        marks[1] = 89;
+        marks[2] = 90;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
