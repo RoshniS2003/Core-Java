d917675056c0c2b60f3095d1380902d030866862
Roshni <roshnisahu11222@gmail.com>
2026-07-23 10:50:43 +0530

    Input Values from the User

---
+++ 4_UserInput/Input.java
@@ -0,0 +1,15 @@
+// 4th
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Roshni Sahu
+        // String name = sc.next();  // next print only Roshni
+        String name = sc.nextLine(); // nextline print Roshni Sahu
+        //nextInt()
+        //nextFloat()
+        System.out.println(name);
+    }
+}
