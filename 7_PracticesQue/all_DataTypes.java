7d85d2236ec0dd3ed8105856925dbd9360cd3a4c
Roshni <roshnisahu11222@gmail.com>
2026-07-25 12:19:22 +0530

    Added Datatypes Concepts

---
+++ 7_PracticesQue/all_DataTypes.java
@@ -0,0 +1,18 @@
+public class all_DataTypes {
+    public static void main(String[] args) {
+        int age = 18;
+        float Pi = 3.14f; // In java we use float not const
+        double accountbalance = 258.27;
+        boolean isLoggedIn = true;
+        boolean hasPassedExam = false;
+        char userGrade = 'A';
+        String name = "Roshni";
+        long worldPopulation = 10000000L;
+
+        System.out.println("Age(Int): " + age);
+        System.out.println("PI(Float): " +Pi);
+        System.out.println("Account Balance(Double): " + accountbalance);
+        System.out.println("Is Logged In(Boolean): " + isLoggedIn);
+        System.out.println("Has Passed Exam(Boolean): " + hasPassedExam);
+    }
+}
