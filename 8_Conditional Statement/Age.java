158136049f52f40c647769239a5536c91860499f
Roshni <roshnisahu11222@gmail.com>
2026-08-25 23:47:56 +0530

    Check Adult or not using if-else Statement

---
+++ 8_Conditional Statement/Age.java
@@ -0,0 +1,10 @@
+public class Age {
+    public static void main(String[] args) {
+        int age = 98;
+        if (age >= 18) {
+            System.out.println("Adult");
+        } else {
+              System.out.println("Not Adult");
+        }
+    }
+}
