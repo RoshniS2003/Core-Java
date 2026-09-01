db44443c2e2b6d7624572e607918aab4fc425991
Roshni <roshnisahu11222@gmail.com>
2026-09-01 20:50:46 +0530

    Learn Loop Concepts

---
+++ 9_Loops/loop.java
@@ -0,0 +1,9 @@
+public class loop {
+    public static void main(String[] args) {
+        // if we what to print 20 times so its not possible to paste again again
+        // so we use loop:--->>> for , while and do-while loop
+        System.out.println("Hello Roshni");
+        System.out.println("Hello Roshni");
+        System.out.println("Hello Roshni");
+    }
+}
