1aeb4aa92e5ab7f42a8b0615940335ac04bd11ff
Roshni <roshnisahu11222@gmail.com>
2026-09-02 23:37:55 +0530

    Learn how to print 0 to 10 number

---
+++ 9_Loops/numPrint.java
@@ -0,0 +1,9 @@
+public class numPrint {
+    public static void main(String[] args) {
+        // Dry Run
+        // for(int i=0 ; i <=10 ; i++){
+    for(int i=0 ; i <=10 ; i++){
+        System.out.print(i + " ");
+    }
+ }    
+}
