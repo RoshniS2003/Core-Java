ececdd658195781c692d1802ae20cbc68aaf7919
Roshni <roshnisahu11222@gmail.com>
2026-09-03 23:50:36 +0530

    learn while and do-while loop statement

---
+++ 9_Loops/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+
+        // while loop
+        int i = 5;
+        while (i < 4) {   // false the condition
+            System.out.println("Roshni Sahu");
+        }
+
+        // do-while loop
+         do{
+            System.out.println("Roshni Sahu");   // first print the msg
+            i++;
+        }while (i < 4);  // then condition check + always add terminal ;
+    }
+}
